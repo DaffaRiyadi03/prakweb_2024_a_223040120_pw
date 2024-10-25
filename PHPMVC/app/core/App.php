015a95be7514d6cf13ff55605f97f34eb556b8d7
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-10-26 00:22:27 +0700

    Membuat Aplikasi MVC dengan PHP #3 Routing

--- PHPMVC/app/core/App.php
+++ PHPMVC/app/core/App.php
@@ -4,7 +4,18 @@ class App
 {
     public function __construct()
     {
-        echo 'OKE!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/'); // Menghilangkan slash di akhir URL
+            $url = filter_var($url, FILTER_SANITIZE_URL); // Sanitasi URL
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
 

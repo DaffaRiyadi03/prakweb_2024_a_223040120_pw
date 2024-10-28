b43f5a056a0f9729c5b38c07ab2499bb1cc51b7e
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-10-28 11:23:53 +0700

    Membuat Aplikasi MVC dengan PHP #7 Model

--- PHPMVC/app/views/home/index.php
+++ PHPMVC/app/views/home/index.php
@@ -3,7 +3,7 @@
     <div class="jumbotron mt-5">
         <h1 class="display-4"> Selamat Datang di Website Saya!</h1>
         <p class="lead">
-            Halo, nama saya Daffa
+            Halo, nama saya <?= $data['nama']; ?>
         </p>
         <hr class="my-4">
         <a href="#" class="btn btn-primary btn-lg" role="button">Hayu</a>
